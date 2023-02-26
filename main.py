de2046cde695052bc061990bbe6c6c9b845f5e52
Alex Shishkin <alexsischin@gmail.com>
2023-02-26 17:43:55 +0200

    Data splitting and transformation

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MaxAbsScaler
+
 from sparse_utils import read_arff
 
 
+def get_data():
+    _, df = read_arff('dataset/Pre+TW-Ds.arff')
+    matrix = df.sparse.to_coo().tocsr()
+    X, y = matrix[:, 1:], matrix[:, 0]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+    return X_train, X_test, y_train, y_test
+
+
 def main():
-    attributes, df = read_arff('dataset/Pre+TW-Ds.arff')
-    print(df)
+    X_train, X_test, y_train, y_test = get_data()
+
+    pipe = make_pipeline(MaxAbsScaler()).fit(X_train)
+    X_train_transformed = pipe.transform(X_train)
+    X_test_transformed = pipe.transform(X_test)
+
+    print(X_train_transformed)
 
 
 if __name__ == '__main__':
